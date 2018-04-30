0efc0e2360c9d6be1b9ce4f503e8a74df6f91712
LENOVO <LENOVO@DESKTOP-HHHBJO7>
2018-04-30 14:46:33 +0100

    Modification de clients (historique) + Ajout de services

--- src/main/java/ActionServlet.java
+++ src/main/java/ActionServlet.java
@@ -1,10 +1,11 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import fr.insalyon.dasi.proactif.DAO.JpaUtil;
+import fr.insalyon.dasi.proactif.metier.OM.Personne;
+import fr.insalyon.dasi.proactif.metier.Service.ServiceMetier;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,16 +30,19 @@ public class ActionServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
         try (PrintWriter out = response.getWriter()) {
             //Service s= new Service();
             String todo =request.getParameter("action");
             if ("connecter".equals(todo)){
                 String login= request.getParameter("login");
                 String password= request.getParameter("password");
-                //Personne p=ServiceMetier.chercherPersonneMailEtMdp(login,password);
-                //printDetailPersonne(out,p);
+                Personne p=ServiceMetier.chercherPersonneMailEtMdp(login,password);
+                if(p==null){
+                    printDetailConnection(out,"KO");
+                }else{
+                    printDetailConnection(out,"OK");
+                }
             }
 //            if (q==null){
 //                List<Service.Personne> personnes = s.consulterListePersonnes();
@@ -76,20 +80,38 @@ public class ActionServlet extends HttpServlet {
 //        out.println(gson.toJson(container));
 //    }
 //    
-//    protected void printDetailPersonne(PrintWriter out, Personne p){
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        JsonObject jsonPersonne= new JsonObject();
-//        jsonPersonne.addProperty("id",p.getId());
-//        jsonPersonne.addProperty("civilite",p.getCivilite());
-//        jsonPersonne.addProperty("nom",p.getNom());
-//        jsonPersonne.addProperty("prenom",p.getPrenom());
-//        jsonPersonne.addProperty("numtel",p.getNumTel());
-//        jsonPersonne.addProperty("mail",p.getMail());
-//        jsonPersonne.addProperty("adressePost",p.getAdressePost());
-//        JsonObject container = new JsonObject();
-//        container.add("personne",jsonPersonne);
-//        out.println(gson.toJson(container)); 
-//    }
+    protected void printDetailConnection(PrintWriter out, String connection){
+        Gson gson= new GsonBuilder().setPrettyPrinting().create();
+        JsonObject container = new JsonObject();
+        container.addProperty("Authentification",connection);
+        out.println(gson.toJson(container));
+    }
+    
+    protected void printDetailPersonne(PrintWriter out, Personne p){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonObject jsonPersonne= new JsonObject();
+        jsonPersonne.addProperty("id",p.getId());
+        jsonPersonne.addProperty("civilite",p.getCivilite());
+        jsonPersonne.addProperty("nom",p.getNom());
+        jsonPersonne.addProperty("prenom",p.getPrenom());
+        jsonPersonne.addProperty("numtel",p.getNumTel());
+        jsonPersonne.addProperty("mail",p.getMail());
+        jsonPersonne.addProperty("adressePost",p.getAdressePost());
+        JsonObject container = new JsonObject();
+        container.add("personne",jsonPersonne);
+        out.println(gson.toJson(container)); 
+    }
+
+    @Override
+    public void init() throws ServletException {
+        JpaUtil.init();
+    }
+
+    @Override
+    public void destroy() {
+        JpaUtil.destroy();
+    }
+    
     
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
